809bfb2464ce2b0485f5c1993f1e7858483e2b20
Masami_Nakaoka <neti0326@gmail.com>
2019-01-10 21:21:39 +0900

    httpリクエストの作成中

--- repository.go
+++ repository.go
@@ -6,10 +6,19 @@ import (
 	"github.com/urfave/cli"
 )
 
+const (
+    baseUrl = "https//api.bitbucket.org/2.0/repositories/"
+)
+
 func Repository(c *cli.Context) error {
 	userName := c.App.Metadata["bitbucketUserName"]
 	token := c.App.Metadata["bitbucketToken"]
-	fmt.Println(userName)
-	fmt.Println(token)
+	
+	endPoint := baseUrl + userName + "/"
+	
+	req, err := http.NewRequest("GET", endPoint, nil)
+	if err != nil {
+		return err
+	}
 	return nil
 }
